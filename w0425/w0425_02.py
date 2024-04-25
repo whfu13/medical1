ad7a047eef49965bf37c6a7cfdcc3f8656e88911
김정수 <whfu13@naver.com>
2024-04-25 18:28:12 +0900

    수업내용

---
+++ w0425/w0425_02.py
@@ -0,0 +1,17 @@
+import requests
+import time
+import random
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36","Accept-Language":"ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"}
+
+url = "https://www.naver.com"
+
+# 브라우저 열기
+browser = webdriver.Chrome()
+browser.maximize_window() # 창 최대화
+browser.get(url)
+
